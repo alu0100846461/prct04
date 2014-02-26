3c13a2336cd9b9391ea8374ac9bb448974c2b319
Alba Crespo Perez <alu0100846461@ull.edu.es>
2014-02-26 14:07:33 +0000

    Realizados ejercicios 35 y 36

---
+++ src/ej35.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+#!encoding: UTF-8
+
+for i in range (0,5):
+   for j in range (0,3):
+      print i, j
---
+++ src/ej36.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+#!encoding: UTF-8
+
+for i in range (0,5):
+   for j in range (i,5):
+      print i, j
