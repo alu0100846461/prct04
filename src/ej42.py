9abb4462e60aff27aceab016040c8047743721ca
Alba Crespo Perez <alu0100846461@ull.edu.es>
2014-02-26 17:03:50 +0000

    Realizado y corregido ejercicio 42

---
+++ src/ej42.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+#!encoding: UTF-8
+
+def es_perfecto(n):
+   sumatorio = 0
+   for i in range (1,n):   
+      if n % i == 0:
+         sumatorio += i
+   return (sumatorio == n)
+
+def tabla_perfectos(m):
+   for i in range (1, m+1):
+      if es_perfecto(i):
+         print i, 'es perfecto'
+
+x = int(raw_input('Introduzca un número: '))
+tabla_perfectos(x)
