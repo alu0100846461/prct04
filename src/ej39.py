3eb41ce17d599a980ca6be84804bc1ba828e3550
Alba Crespo Perez <alu0100846461@ull.edu.es>
2014-02-26 16:46:04 +0000

    Realizados ejercicios 39 y 40

---
+++ src/ej39.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+#!encoding: UTF-8
+
+for i in range (0,4):
+   for j in range (0,4):
+      for k in range (i,j):
+         print i, j, k
---
+++ src/ej40.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python
+#!encoding: UTF-8
+
+for i in range (1,5):
+   for j in range (0,10,i):
+      print i, j
